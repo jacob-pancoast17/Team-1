b319f5746b2ecca97a87c569356f6a18334954ff
Jacob Pancoast <jacob.pancoast17@gmail.com>
2025-11-11 13:48:44 -0500

    broke even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,15 @@
+# bugs added
+
 with open("numbers.html", "w") as f:
-    f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
+    f.write("<html>\n\n<title>List of Numbers</title>\n\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    for i in range(1, 50):
+    for i in range(1, 100):
         if i % 2 == 0:
             g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         if i % 3 == 0:
             g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
 
-with open("numbers.html"") as f:
+with open("number.html"") as f:
     print(f.read())
     
