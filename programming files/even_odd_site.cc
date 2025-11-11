99a54f313717b089aaac3fca93749f72ecc7c1f0
Jacob Pancoast <jacob.pancoast17@gmail.com>
2025-11-11 13:45:32 -0500

    Broke even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -2,12 +2,17 @@
 #include <fstream>
 using namespace std;
 
+// Did not close input file
+// Loop increments wrong
+// Modulo in the for loop is wrong 
+// Broken by Jacob
+
 int main() {
     ofstream file;
     file.open("numbers.html");
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
-    for (int i = 1; i <= 50; i++) {
+    for (int i = 1; i < 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
@@ -19,6 +24,5 @@ int main() {
     file.close();
     ifstream input("numbers.html");
     cout << input.rdbuf();
-    input.close();
     return 0;
 }
